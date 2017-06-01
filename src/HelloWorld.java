915395848835d94e1883ef950da6ad17d942a915
Jérémie Pierson <jeremie.pierson@nantesmetropole.fr>
2017-06-01 07:51:20 +0000

    Trigger a nother Docker automated build

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ import java.util.Map;
 public class HelloWorld {
     public static void main(String[] args) {
         Map<String, String> env = System.getenv();
-        System.out.format("Hello, automated %s!%n", env.get("HELLO"));
+        System.out.format("Hello, automated v1.2 %s!%n", env.get("HELLO"));
     }
 }
